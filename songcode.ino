9677c14384b3b02469bfe707b16e4b1b1b91f076
Jasminev297 <jasminev297@gmail.com>
2023-04-12 13:28:44 -0500

    Tempo Added

--- songcode.ino
+++ songcode.ino
@@ -4,6 +4,11 @@ void setup() {
   // Aurora's code
 
   // Jasmine's code
+int tempo = 1800
+int dotquarter = tempo/2;
+int quarter = tempo/3;
+int sixteenth = tempo/6;
+int dothalf = tempo * (10/9)
 
   // Malika's code
 
